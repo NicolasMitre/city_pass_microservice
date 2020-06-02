448bce7ad53e406b7f25eea6798d7c68489ea093
NicolasMitre <nicolasmelluso7@gmail.com>
2020-06-02 10:32:25 -0300

    CompÂplete skelleton base

---
+++ src/main/java/net/avalith/city_pass/CityPassApplication.java
@@ -0,0 +1,13 @@
+package net.avalith.city_pass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityPassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityPassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/avalith/city_pass/CityPassApplicationTests.java
@@ -0,0 +1,13 @@
+package net.avalith.city_pass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
