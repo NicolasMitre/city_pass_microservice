19b68f20cf1a8e7d210829e14d5c90b62558f528
Federico Anastasi <fede_3775@hotmail.com>
2020-06-05 12:52:43 -0300

    Base structure

--- src/main/java/net/avalith/city_pass/controllers/ControllerAdvice.java
+++ src/main/java/net/avalith/city_pass/controllers/ControllerAdvice.java
@@ -3,17 +3,10 @@ package net.avalith.city_pass.controllers;
 import net.avalith.city_pass.dto.ErrorResponseDto;
 import net.avalith.city_pass.exceptions.CityNotFoundException;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import javax.validation.ValidationException;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static net.avalith.city_pass.utils.Constants.CITY_NOT_FOUND_MESSAGE;
 
