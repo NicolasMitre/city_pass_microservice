fd09832f6accd886c715b051dc4bda63b8047fec
NicolasMitre <nicolasmelluso7@gmail.com>
2020-06-06 15:16:47 -0300

    Ã[Working] Added the endpoint to get all roles and add a new role

--- src/main/java/net/avalith/city_pass/services/RoleService.java
+++ src/main/java/net/avalith/city_pass/services/RoleService.java
@@ -1,11 +1,14 @@
 package net.avalith.city_pass.services;
 
 import lombok.RequiredArgsConstructor;
+import net.avalith.city_pass.dto.RoleDto;
+import net.avalith.city_pass.models.City;
 import net.avalith.city_pass.models.Role;
 import net.avalith.city_pass.repositories.RoleRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @Service
@@ -16,5 +19,19 @@ public class RoleService {
 
     public List<Role> getAllRole() {
         return roleRepository.findAll();
+    }
+
+    public URI createRole(RoleDto roleDto) {
+        Role role = Role.builder().name(roleDto.getName()).build();
+        role = roleRepository.save(role);
+        return getLocation(role);
+    }
+
+    private URI getLocation(Role role) {
+        return ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(role.getId())
+                .toUri();
     }
 }
