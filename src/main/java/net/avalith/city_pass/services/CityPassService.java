e0371ceb6abeff4e840433f0be6d33b97e2da550
NicolasMitre <nicolasmelluso7@gmail.com>
2020-06-09 12:42:32 -0300

    [Working] Add cityPass service

--- src/main/java/net/avalith/city_pass/services/CityPassService.java
+++ src/main/java/net/avalith/city_pass/services/CityPassService.java
@@ -1,10 +1,14 @@
 package net.avalith.city_pass.services;
 
 import lombok.RequiredArgsConstructor;
+import net.avalith.city_pass.dto.CityPassDto;
+import net.avalith.city_pass.exceptions.CityPassNotFoundException;
 import net.avalith.city_pass.models.CityPass;
 import net.avalith.city_pass.repositories.CityPassRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @Service
@@ -13,8 +17,57 @@ public class CityPassService {
 
     private final CityPassRepository cityPassRepository;
 
-    public List<CityPass> getAllCityPasses() {
-        return cityPassRepository.findAll();
+    public List<CityPass> getAllCityPassesActives() {
+        return cityPassRepository.findAllByStatus(true);
+    }
+
+    public URI createCityPass(CityPassDto cityPassDto) {
+        CityPass cityPass = CityPass.builder().name(cityPassDto.getName()).build();
+        cityPass = cityPassRepository.save(cityPass);
+        return getLocation(cityPass);
+    }
+
+    private URI getLocation(CityPass cityPass) {
+        return ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(cityPass.getId())
+                .toUri();
+    }
+
+    public CityPassDto getById(Integer idCityPass) {
+        CityPass cityPass = cityPassRepository.findByIdAndIsActive(idCityPass, true)
+                .orElseThrow(CityPassNotFoundException::new);
+        CityPassDto cityPassDto = new CityPassDto();
+        cityPassDto.fromCityPass(cityPass);
+        return cityPassDto;
+    }
+
+    public CityPass updateCityPass(Integer idCityPass, CityPassDto cityPassDto) {
+        CityPass cityPass = cityPassRepository.findByIdAndIsActive(idCityPass, true)
+                .orElseThrow(CityPassNotFoundException::new);
+        update(cityPass, cityPassDto);
+        cityPass = cityPassRepository.save(cityPass);
+        return CityPass.builder()
+                .name(cityPass.getName())
+                .description(cityPass.getDescription())
+                .price(cityPass.getPrice())
+                .days(cityPass.getDays())
+                .build();
+
+    }
+
+    private void update(CityPass cityPass, CityPassDto cityPassDto){
+        cityPass.setName(cityPassDto.getName());
+        cityPass.setDescription(cityPassDto.getDescription());
+        cityPass.setPrice(cityPassDto.getPrice());
+        cityPass.setDays(cityPassDto.getDays());
+    }
+
+    public void deleteCityPass(Integer idCityPass) {
+        CityPass cityPass = cityPassRepository.findByIdAndIsActive(idCityPass, true)
+                .orElseThrow(CityPassNotFoundException::new);
+        cityPassRepository.logicDelete(idCityPass);
     }
 }
 
