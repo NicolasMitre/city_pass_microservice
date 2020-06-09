b37ae7565820eaf605b6933920121502f61086fa
NicolasMitre <nicolasmelluso7@gmail.com>
2020-06-09 12:42:56 -0300

    [Working] add citypass repository

--- src/main/java/net/avalith/city_pass/repositories/CityPassRepository.java
+++ src/main/java/net/avalith/city_pass/repositories/CityPassRepository.java
@@ -2,8 +2,24 @@ package net.avalith.city_pass.repositories;
 
 import net.avalith.city_pass.models.CityPass;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CityPassRepository extends JpaRepository<CityPass, Integer> {
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE city_pass SET is_active = false where id_city_pass = ?1", nativeQuery = true)
+    void logicDelete(Integer idCityPass);
+
+    Optional<CityPass> findByIdAndIsActive(Integer idCityPass, boolean b);
+
+    @Query(value = "SELECT * from city_pass where is_active = :status", nativeQuery = true)
+    List<CityPass> findAllByStatus(@Param("status") Boolean b);
 }
