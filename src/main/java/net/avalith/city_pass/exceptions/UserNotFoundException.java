77eb269c773ca93f75fbdaa4655b35c995dc1432
MauroCR <ZeroCooler>
2020-06-09 19:11:49 -0300

    [FIX] add UserNotFound exception to Controlleradvice

--- src/main/java/net/avalith/city_pass/exceptions/UserNotFoundException.java
+++ src/main/java/net/avalith/city_pass/exceptions/UserNotFoundException.java
@@ -1,5 +1,8 @@
 package net.avalith.city_pass.exceptions;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+
 public class UserNotFoundException extends RuntimeException{
     public UserNotFoundException(){super();}
 }
