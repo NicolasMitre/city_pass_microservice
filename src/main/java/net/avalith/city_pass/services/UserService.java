afe33c60dc3e32edb84cacbcdea452d53051b2b2
MauroCR <ZeroCooler>
2020-06-09 10:55:42 -0300

    [Feature] Task #25 Create UserService

--- src/main/java/net/avalith/city_pass/services/UserService.java
+++ src/main/java/net/avalith/city_pass/services/UserService.java
@@ -1,15 +1,23 @@
 package net.avalith.city_pass.services;
 
 import lombok.RequiredArgsConstructor;
+import net.avalith.city_pass.dto.RoleDto;
 import net.avalith.city_pass.dto.UserDto;
+import net.avalith.city_pass.exceptions.RoleNotFoundException;
 import net.avalith.city_pass.exceptions.UserNotFoundException;
+import net.avalith.city_pass.models.Role;
 import net.avalith.city_pass.models.User;
 import net.avalith.city_pass.repositories.RoleRepository;
 import net.avalith.city_pass.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.swing.text.html.Option;
 import java.net.URI;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -21,24 +29,36 @@ public class UserService {
     public List<UserDto> getAll() {
         return this.userRepository.findAll()
                 .stream()
-                .map(user -> new UserDto().fromUser(user))
+                .map(user -> new UserDto(user))
                 .collect(Collectors.toList());
     }
 
     public UserDto getById(Integer id) {
         User user = userRepository.findById(id)
                 .orElseThrow(UserNotFoundException::new);
-
-        UserDto userDto = new UserDto();
-        userDto.fromUser(user);
+        System.out.println(user);
+        UserDto userDto = new UserDto(user);
         return userDto;
     }
 
     public URI createUser(UserDto userDto) {
-        User user = User.builder().name(userDto.getName()).username(userDto.getUsername())..build();
+
+        User user = User.builder().name(userDto.getName())
+                                  .username(userDto.getUsername())
+                                  .roles(findRoles(userDto.getRoles()))
+                        .build();
         user = userRepository.save(user);
         return getLocation(user);
     }
+
+    private Set<Role> findRoles(List<String> roles){
+        Set<Role> roleSet = new HashSet<>();
+        for(String rolename : roles){
+            roleSet.add(this.roleRepository.findByName(rolename).orElseThrow(RoleNotFoundException::new));
+        }
+        return roleSet;
+    }
+
 
     private URI getLocation(User user) {
         return ServletUriComponentsBuilder
