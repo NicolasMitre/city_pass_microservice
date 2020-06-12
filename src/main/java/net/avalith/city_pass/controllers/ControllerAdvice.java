1434a5214a48b57b0c3a5012c703c910c223f636
MauroCR <ZeroCooler>
2020-06-12 10:48:06 -0300

    [Feature]Task#5 TheaterPlay Service + CRUD

--- src/main/java/net/avalith/city_pass/controllers/ControllerAdvice.java
+++ src/main/java/net/avalith/city_pass/controllers/ControllerAdvice.java
@@ -3,6 +3,7 @@ package net.avalith.city_pass.controllers;
 import net.avalith.city_pass.dto.ErrorResponseDto;
 import net.avalith.city_pass.exceptions.CityNotFoundException;
 import net.avalith.city_pass.exceptions.RoleNotFoundException;
+import net.avalith.city_pass.exceptions.TheaterPlayNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import static net.avalith.city_pass.utils.Constants.CITY_NOT_FOUND_MESSAGE;
 import static net.avalith.city_pass.utils.Constants.ROLE_NOT_FOUND_MESSAGE;
+import static net.avalith.city_pass.utils.Constants.THEATERPLAY_NOT_FOUND_MESSAGE;
 
 @RestControllerAdvice
 public class ControllerAdvice extends ResponseEntityExceptionHandler {
@@ -25,5 +27,11 @@ public class ControllerAdvice extends ResponseEntityExceptionHandler {
     @ExceptionHandler(RoleNotFoundException.class)
     public ErrorResponseDto handleLoginException(RoleNotFoundException exc) {
         return new ErrorResponseDto(1, ROLE_NOT_FOUND_MESSAGE);
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(TheaterPlayNotFoundException.class)
+    public ErrorResponseDto handleLoginException(TheaterPlayNotFoundException exc){
+        return new ErrorResponseDto(1,THEATERPLAY_NOT_FOUND_MESSAGE);
     }
 }
--- src/main/java/net/avalith/city_pass/services/TheaterPlayService.java
+++ src/main/java/net/avalith/city_pass/services/TheaterPlayService.java
@@ -2,6 +2,7 @@ package net.avalith.city_pass.services;
 
 import lombok.RequiredArgsConstructor;
 import net.avalith.city_pass.dto.TheaterPlayDto;
+import net.avalith.city_pass.exceptions.TheaterPlayNotFoundException;
 import net.avalith.city_pass.models.TheaterPlay;
 import net.avalith.city_pass.repositories.TheaterPlayRepository;
 import org.springframework.stereotype.Service;
@@ -13,11 +14,61 @@ import java.util.stream.Collectors;
 @Service
 public class TheaterPlayService {
     private final TheaterPlayRepository theaterPlayRepository;
+    private final CityService cityService;
 
     public List<TheaterPlayDto> getAll() {
         return this.theaterPlayRepository.findAllByisActive(Boolean.TRUE)
                 .stream()
                 .map(TheaterPlayDto::new)
                 .collect(Collectors.toList());
+    }
+
+    public TheaterPlayDto createTheaterPlay(TheaterPlayDto theaterPlayDto) {
+        TheaterPlay theaterPlay = TheaterPlay.builder()
+                                .city(findByNameAndIsActive(theaterPlayDto.getCityName(),Boolean.TRUE))
+                                .theaterName(theaterPlayDto.getTheaterName())
+                                .name(theaterPlayDto.getName())
+                                .durationInMinutes(theaterPlayDto.getDurationInMinutes())
+                                .price(theaterPlayDto.getPrice())
+                                .playDate(theaterPlayDto.getPlayDate())
+                                .description(theaterPlayDto.getDescription())
+                                .isActive(Boolean.TRUE)
+                                .build();
+        return new TheaterPlayDto(theaterPlayRepository.save(theaterPlay));
+    }
+
+    public TheaterPlayDto getById(Integer id) {
+        return theaterPlayRepository.findByIdAndIsActive(id,true)
+                .map(TheaterPlayDto::new)
+                .orElseThrow(TheaterPlayNotFoundException::new);
+    }
+
+
+    public TheaterPlayDto updateTheaterPlay(Integer id, TheaterPlayDto theaterPlayDto) {
+        return this.theaterPlayRepository.findByIdAndIsActive(id, true)
+                .map(theaterPlay -> update(theaterPlay,theaterPlayDto))
+                .map(theaterPlayRepository::save)
+                .map(TheaterPlayDto::new)
+                .orElseThrow(TheaterPlayNotFoundException::new);
+    }
+
+    private TheaterPlay update(TheaterPlay theaterPlay, TheaterPlayDto theaterPlayDto){
+        theaterPlay.setCity(cityService.findByNameAndIsActive(theaterPlayDto.getCityName(),Boolean.TRUE));
+        theaterPlay.setName(theaterPlayDto.getName());
+        theaterPlay.setDurationInMinutes(theaterPlayDto.getDurationInMinutes());
+        theaterPlay.setPrice(theaterPlayDto.getPrice());
+        theaterPlay.setPlayDate(theaterPlayDto.getPlayDate());
+        theaterPlay.setDescription(theaterPlayDto.getDescription());
+
+        return theaterPlay;
+    }
+
+
+    public TheaterPlayDto logicDelete(Integer id) {
+        TheaterPlayDto theaterPlayDto = this.theaterPlayRepository.findByIdAndIsActive(id,Boolean.TRUE)
+                .map(TheaterPlayDto::new)
+                .orElseThrow(TheaterPlayNotFoundException::new);
+        this.theaterPlayRepository.deleteById(id);
+        return theaterPlayDto;
     }
 }
