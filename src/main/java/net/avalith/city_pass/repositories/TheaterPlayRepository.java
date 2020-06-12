b60dd7e69f7436686fe3fbc5641b27bdaab4fce6
MauroCR <ZeroCooler>
2020-06-12 10:48:59 -0300

    [Feature]Task#8 TheaterPlay ReÂpository + CRUD

--- src/main/java/net/avalith/city_pass/repositories/TheaterPlayRepository.java
+++ src/main/java/net/avalith/city_pass/repositories/TheaterPlayRepository.java
@@ -2,9 +2,19 @@ package net.avalith.city_pass.repositories;
 
 import net.avalith.city_pass.models.TheaterPlay;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 public interface TheaterPlayRepository extends JpaRepository<TheaterPlay, Integer> {
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE user SET is_active = false where id_user = ?1", nativeQuery = true)
+    void deleteById(Integer id);
     List<TheaterPlay> findAllByisActive(Boolean status);
+
+    Optional<TheaterPlay> findByIdAndIsActive(Integer id, boolean b);
 }
