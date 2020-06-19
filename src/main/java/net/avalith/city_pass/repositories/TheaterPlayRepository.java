cfcd1212bc5b39b9b55869fec91fed2d0b0e0d26
MauroCR <ZeroCooler>
2020-06-19 11:14:47 -0300

    [Fix] TheaterPlayRepository

--- src/main/java/net/avalith/city_pass/repositories/TheaterPlayRepository.java
+++ src/main/java/net/avalith/city_pass/repositories/TheaterPlayRepository.java
@@ -10,17 +10,12 @@ import java.util.List;
 import java.util.Optional;
 
 public interface TheaterPlayRepository extends JpaRepository<TheaterPlay, Integer> {
-    @Transactional
-    @Modifying
-    @Query(value = "UPDATE user SET is_active = false where id_user = ?1", nativeQuery = true)
-    void deleteById(Integer id);
-    List<TheaterPlay> findAllByisActive(Boolean status);
-
-    @Transactional
     @Query(value ="SELECT * FROM theater_play t inner join cities c on c.id_city = t.cities_id WHERE t.is_active = ?2 AND c.name =?1 ",nativeQuery = true)
-    Optional<List<TheaterPlay>> findByCityNameAndIsActive(String cityName,Boolean value);
+    List<TheaterPlay> findByCityNameAndIsActive(String cityName,Boolean value);
 
     Optional<TheaterPlay> findByIdAndIsActive(Integer id, Boolean b);
 
-    Optional<TheaterPlay> findByNameAndIsActive(String theaterPlayName, Boolean aTrue);
+    Optional<TheaterPlay> findByNameAndIsActive(String theaterPlayName, Boolean status);
+
+    List<TheaterPlay> findAllByisActive(Boolean status);
 }
