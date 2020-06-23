7ea221966f7072e2687db5b0359ba22fa7a9f2d9
Federico Anastasi <fede.3328@gmail.com>
2020-06-23 09:59:33 -0300

    [Working] New Message Error

--- src/main/java/net/avalith/city_pass/utils/Constants.java
+++ src/main/java/net/avalith/city_pass/utils/Constants.java
@@ -2,6 +2,7 @@ package net.avalith.city_pass.utils;
 
 public class Constants {
     public static String CITY_NOT_FOUND_MESSAGE = "City not found";
+    public static String CITY_NAME_ALREADY_USED_MESSAGE = "That name for the city is already used";
     public static String ROLE_NOT_FOUND_MESSAGE = "Role not found";
     public static String USER_NOT_FOUND_MESSAGE = "User not found";
     public static String CITY_PASS_NOT_FOUND_MESSAGE = "City pass not found";
