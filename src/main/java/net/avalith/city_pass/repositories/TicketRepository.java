7f398b585326743b4659c639e3d81527bc313709
Federico Anastasi <fede.3328@gmail.com>
2020-07-06 16:00:34 -0300

    Add ticket service repository

--- src/main/java/net/avalith/city_pass/repositories/TicketRepository.java
+++ src/main/java/net/avalith/city_pass/repositories/TicketRepository.java
@@ -2,6 +2,11 @@ package net.avalith.city_pass.repositories;
 
 import net.avalith.city_pass.models.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket,Integer> {
+    @Query("SELECT t from Ticket t where t.purchase.id = ?1")
+    List<Ticket> getAllTicketsByPurchaseId(Integer idPurchase);
 }
--- src/main/java/net/avalith/city_pass/services/TicketService.java
+++ src/main/java/net/avalith/city_pass/services/TicketService.java
@@ -1,29 +1,14 @@
 package net.avalith.city_pass.services;
 
 import lombok.RequiredArgsConstructor;
-import net.avalith.city_pass.dto.TicketDto;
-import net.avalith.city_pass.exceptions.UserNotFoundException;
-import net.avalith.city_pass.models.City;
-import net.avalith.city_pass.models.CityPass;
-import net.avalith.city_pass.models.CityPassTicket;
-import net.avalith.city_pass.models.Excursion;
-import net.avalith.city_pass.models.TheaterPlay;
-import net.avalith.city_pass.models.TheaterPlayTicket;
+import net.avalith.city_pass.dto.request.TicketRequestDto;
+import net.avalith.city_pass.models.Purchase;
 import net.avalith.city_pass.models.Ticket;
 import net.avalith.city_pass.models.factory.TicketFactory;
-import net.avalith.city_pass.repositories.CityPassTicketRepository;
-import net.avalith.city_pass.repositories.TheaterPlayTicketRepository;
 import net.avalith.city_pass.repositories.TicketRepository;
 import org.springframework.stereotype.Service;
 
-import java.math.BigInteger;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 
 
 @Service
@@ -32,10 +17,12 @@ public class TicketService {
     private final TicketFactory ticketFactory;
     private final TicketRepository ticketRepository;
 
-    public Ticket processData(TicketDto ticketDto) {
-        Ticket productBought = ticketFactory.getTicket(ticketDto);
+    public Ticket processTicket(TicketRequestDto ticketRequestDto, Purchase purchase) {
+        Ticket productBought = ticketFactory.getTicket(ticketRequestDto, purchase);
         return ticketRepository.save(productBought);
     }
 
-
+    public List<Ticket> getByPurchaseId(Integer idPurchase) {
+        return ticketRepository.getAllTicketsByPurchaseId(idPurchase);
+    }
 }
