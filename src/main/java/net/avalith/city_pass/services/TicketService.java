bc33e271faaec9d51911bbb8794d55ce13d77de3
Federico Anastasi <fede.3328@gmail.com>
2020-07-23 11:19:47 -0300

    [Working] PayPalApi integration

--- src/main/java/net/avalith/city_pass/services/TicketService.java
+++ src/main/java/net/avalith/city_pass/services/TicketService.java
@@ -2,12 +2,14 @@ package net.avalith.city_pass.services;
 
 import lombok.RequiredArgsConstructor;
 import net.avalith.city_pass.dto.request.TicketRequestDto;
-import net.avalith.city_pass.models.Purchase;
 import net.avalith.city_pass.models.Ticket;
 import net.avalith.city_pass.models.factory.TicketFactory;
 import net.avalith.city_pass.repositories.TicketRepository;
 import org.springframework.stereotype.Service;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -17,9 +19,12 @@ public class TicketService {
     private final TicketFactory ticketFactory;
     private final TicketRepository ticketRepository;
 
-    public Ticket processTicket(TicketRequestDto ticketRequestDto, Purchase purchase) {
-        Ticket productBought = ticketFactory.getTicket(ticketRequestDto, purchase);
-        return ticketRepository.save(productBought);
+    public Ticket processTicket(TicketRequestDto ticketRequestDto, LocalDateTime purchaseDate) {
+        return ticketFactory.getTicket(ticketRequestDto,purchaseDate);
+    }
+
+    public List<Ticket> persistTicket(@NotEmpty List<@Valid Ticket> tickets){
+        return ticketRepository.saveAll(tickets);
     }
 
     public List<Ticket> getByPurchaseId(Integer idPurchase) {
