e03e0f9ff7ae9854055e69546949b627773128eb
Federico Anastasi <fede.3328@gmail.com>
2020-07-23 11:04:03 -0300

    Change messages to static final

--- src/main/java/net/avalith/city_pass/utils/Constants.java
+++ src/main/java/net/avalith/city_pass/utils/Constants.java
@@ -1,17 +1,18 @@
 package net.avalith.city_pass.utils;
 
 public class Constants {
-    public static String CITY_NOT_FOUND_MESSAGE = "City not found";
-    public static String ROLE_NOT_FOUND_MESSAGE = "Role not found";
-    public static String USER_NOT_FOUND_MESSAGE = "User not found";
-    public static String CITY_PASS_NOT_FOUND_MESSAGE = "City pass not found";
-    public static String EXCURSION_NOT_FOUND_MESSAGE = "Excursion not found";
-    public static String EXCURSION_NAME_ALREADY_USED_MESSAGE = "Excursion name is already being used";
-    public static String CITY_PASS_NAME_IS_ALREADY_USED = "The name for the city pass is already used";
-    public static String THEATERPLAY_NOT_FOUND_MESSAGE = "TheaterPlay not found";
-    public static String PURCHASE_NOT_FOUND_MESSAGE = "Purchase not found";
-    public static Long THEATERPLAY_CAPACITY = 100L;
-    public static String THEATERPLAY_TICKET_SOLD_OUT = "All Tickets are Sold Out";
-    public static String THEATERPLAY_REMAINING_TICKET = "We do not have that quantity. Remaining of tickets : ";
-    public static String THEATERPLAY_TICKET_NOT_VALID = "TheaterPlay Ticket quantity is not valid -> ";
+    public static final Long THEATERPLAY_CAPACITY = 100L;
+    public static final String THEATERPLAY_TICKET_SOLD_OUT = "All Tickets are Sold Out";
+    public static final String THEATERPLAY_REMAINING_TICKET = "We do not have that quantity. Remaining of tickets : ";
+    public static final String THEATERPLAY_TICKET_NOT_VALID = "TheaterPlay Ticket quantity is not valid -> ";
+    public static final String CITY_NOT_FOUND_MESSAGE = "City not found";
+    public static final String ROLE_NOT_FOUND_MESSAGE = "Role not found";
+    public static final String USER_NOT_FOUND_MESSAGE = "User not found";
+    public static final String CITY_PASS_NOT_FOUND_MESSAGE = "City pass not found";
+    public static final String EXCURSION_NOT_FOUND_MESSAGE = "Excursion not found";
+    public static final String EXCURSION_NAME_ALREADY_USED_MESSAGE = "Excursion name is already being used";
+    public static final String CITY_PASS_NAME_IS_ALREADY_USED = "The name for the city pass is already used";
+    public static final String THEATERPLAY_NOT_FOUND_MESSAGE = "TheaterPlay not found";
+    public static final String PURCHASE_NOT_FOUND_MESSAGE = "Purchase not found";
+
 }
